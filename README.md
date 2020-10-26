9bdf97bf17836d674c0de67312bfebbddb610fb4
yuby <s30234d@naver.com>
2020-10-26 18:37:42 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# fullstack react-gql-ts
+
+
+- [적용] Typescript
+- [적용] Nodejs
+- [적용] MongoDB
+- [적용] TypeORM
+- [적용] eslin/airbnb-typescript
+- URQL
+- Redis
+- Nextjs
+- TypeGraphQL
+- React
---
+++ src/entity/User.ts
@@ -0,0 +1,20 @@
+import {
+  Entity, ObjectIdColumn, ObjectID, Column,
+} from 'typeorm';
+
+@Entity()
+export class User {
+  @ObjectIdColumn()
+  id: ObjectID;
+
+  @Column()
+  firstName: string;
+
+  @Column()
+  lastName: string;
+
+  @Column()
+  age: number;
+}
+
+export interface User {}
